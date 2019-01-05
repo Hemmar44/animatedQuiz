a19699c509a286410c0832a849b7372bdf8a0d5f
Marcin Hędrzak <marcin.hedrzak@komputronik.pl>
2019-01-05 22:28:42 +0100

    dodawanie i odejmowanie

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <quiz-base></quiz-base>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import QuizBase from './components/QuizBase'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    QuizBase
   }
 }
 </script>
---
+++ src/components/QuizBase.vue
@@ -0,0 +1,111 @@
+<template>
+    <div class="container">
+        <p class="alert alert-warning">{{ question }} </p>
+        <div class="row">
+            <div
+                v-for="select in allAnswers"
+                :key="select"
+                class="col-lg-3"
+            >
+                <button class="btn btn-primary" @click.prevent="checkAnswer(select)">{{ select }}</button></div>
+            </div>
+        <div>
+            {{ score }}
+        </div>
+        <div v-show="message.length">
+            {{ message }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "QuizBase",
+        data() {
+            return {
+                answer: '',
+                allAnswers: [],
+                question: '',
+                score: 0,
+                message: '',
+                points: 3
+            }
+        },
+        created() {
+            this.generateQuestion();
+        },
+        methods: {
+            generateQuestion() {
+                this.mathematics();
+            },
+            mathematics() {
+                let equations = ['+', '-'];
+                this.shuffleArray(equations);
+                switch (equations[0]) {
+                    case '+':
+                        this.sum();
+                        this.generateAnswers();
+                        break;
+                    case '-':
+                       this.minus();
+                       this.generateAnswers();
+                }
+            },
+            sum() {
+                let first = Math.floor(Math.random() * 100);
+                let second = Math.floor(Math.random() * 100);
+                this.answer = first + second;
+                this.question =  first + ' + ' + second + ' ='
+            },
+            minus() {
+                let first = 0;
+                let second = 0;
+                do {
+                    first = Math.floor(Math.random() * 200);
+                    second = Math.floor(Math.random() * 200);
+                    this.answer = first - second;
+                }
+                while ( this.answer <= 0 );
+                this.question =  first + ' - ' + second + ' ='
+            },
+            generateAnswers() {
+                this.allAnswers.push(this.answer);
+                while (this.allAnswers.length < 4) {
+                    let wrongAnswer = Math.floor(Math.random() * 200);
+                    if (!this.allAnswers.includes(wrongAnswer)) {
+                        this.allAnswers.push(wrongAnswer);
+                    }
+                }
+                this.shuffleArray(this.allAnswers);
+            },
+            checkAnswer(select) {
+                if (+select === this.answer) {
+                    this.changeQuestion();
+                } else {
+                    this.points--;
+                    if (this.points === 0 ) {
+                        return this.changeQuestion();
+                    }
+                    this.message = 'Spróbuj jeszcze raz!'
+                }
+            },
+            shuffleArray(array) {
+                for (let i = array.length - 1; i > 0; i--) {
+                    const j = Math.floor(Math.random() * (i + 1));
+                    [array[i], array[j]] = [array[j], array[i]];
+                }
+            },
+            changeQuestion() {
+                this.score += this.points;
+                this.points = 3;
+                this.allAnswers = [];
+                this.message = '';
+                this.generateQuestion();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
